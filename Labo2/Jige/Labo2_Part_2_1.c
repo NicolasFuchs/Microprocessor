2be8420d8d57c3c740135ce9d6198f498a28b088
root <root@localhost.localdomain>
2018-03-02 11:57:05 +0100

    part1

--- Labo2/Jige/Labo2_Part_2_1.c
+++ Labo2/Jige/Labo2_Part_2_1.c
@@ -35,8 +35,8 @@ typedef struct _TEST4{
 } TEST4;
 
 typedef struct _TEST5{
-	char b __attribute__ ((packed));
-	char d __attribute__ ((packed));
+	char b;
+	char d;
 	int a __attribute__ ((aligned (4)));
 	int c __attribute__ ((aligned (4)));
 } TEST5;
